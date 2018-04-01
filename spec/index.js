9697925c0928d3508ba38b39b8e3cf171447380c
sonmaximum <maxjuchheim@gmail.com>
2018-03-31 21:28:25 -0400

    Tweak files
    Tweat root.js, delete unused org.js, update favicon, rm ;s from index.js
    simplify sign-up curl

--- spec/index.js
+++ spec/index.js
@@ -1,5 +1,5 @@
-'use strict';
+'use strict'
 
-const loader = require('../lib/wiring/loader');
+const loader = require('../lib/wiring/loader')
 
-module.exports = loader(__filename);
+module.exports = loader(__filename)
